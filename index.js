60d3cbb05a503246e4514535a68a4665d9e6e334
Marc Lundgren <marclundgren2.0@gmail.com>
2019-04-08 11:12:22 -0700

    can this app listen to all channels?

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const { WebClient } = require('@slack/web-api');
+const { createEventAdapter } = require('@slack/events-api');
+const slackEvents = createEventAdapter(process.env.SLACK_SIGNING_SECRET);
 
 // An access token (from your Slack app or custom integration - xoxp, xoxb)
 const token = process.env.SLACK_TOKEN;
@@ -9,9 +11,14 @@ const web = new WebClient(token);
 const conversationId = '#jorn';
 
 (async () => {
+  // Attach listeners to events by Slack Event "type". See: https://api.slack.com/events/message.im
+  slackEvents.on('message', (event) => {
+    console.log(`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
+  });
+
   // See: https://api.slack.com/methods/chat.postMessage
-  const res = await web.chat.postMessage({ channel: conversationId, text: ':jorn:' });
+  // const res = await web.chat.postMessage({ channel: conversationId, text: ':jorn:' });
 
   // `res` contains information about the posted message
-  console.log('Message sent: ', res.ts);
+  // console.log('Message sent: ', res.ts);
 })();
