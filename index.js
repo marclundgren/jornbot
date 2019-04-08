6c7302c0a5463df153227600f6fab3496d025a83
Marc Lundgren <marclundgren2.0@gmail.com>
2019-04-08 15:26:21 -0700

    try responding only when matching a predefined response set

--- index.js
+++ index.js
@@ -3,24 +3,34 @@ const { RTMClient } = require('@slack/rtm-api');
 const rtm = new RTMClient(process.env.SLACK_TOKEN);
 // var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 
+const responses = {
+  barf: `Douglas had to poop. His butt was all stinky because he had to poop so badly. There was a gross woman named Rebecca who was sunbathing all naked, and she was fat. Douglas walked up to her and said, "I need to poop." "Okay," Rebecca replied, "I like poop." Douglas squatted down over the fat sunbathing lady and went poop.`
+};
+
 // var rtm = new RtmClient();
 rtm.on('message', async (event) => {
   console.log({ event });
 
-  await rtm.sendTyping(event.channel);
-  await (new Promise((resolve) => setTimeout(resolve, 3000)));
+  const { text } = event;
 
-  // Send a message (clears typing indicator)
-  const reply = await rtm.sendMessage(`Welcome to #jorn, <@${event.user}>`, event.channel);
+  const matchingResponse = responses[text.trim()];
 
-  console.log('Message sent successfully', reply.ts);
+  if (matchingResponse) {
+    await rtm.sendTyping(event.channel);
+    await (new Promise((resolve) => setTimeout(resolve, 3000)));
+
+    // Send a message (clears typing indicator)
+    const reply = await rtm.sendMessage(matchingResponse, event.channel);
+
+    console.log('Message sent successfully', reply.ts);
+  }
 });
 
 (async () => {
   // Connect to Slack
-  const { self, team } = await rtm.start();
+  await rtm.start();
 
-  console.log({ self, team });
+  // console.log({ self, team });
 })();
 
 const http = require('http');
