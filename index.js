81b3e75931f41d6282965e22f6bc381cd4068de9
Marc Lundgren <marclundgren2.0@gmail.com>
2019-04-08 10:57:26 -0700

    try the example here https://github.com/slackapi/node-slack-sdk

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
-const { createEventAdapter } = require('@slack/events-api');
-const port = process.env.PORT || 3000;
+const { WebClient } = require('@slack/web-api');
 
-const slackEvents = createEventAdapter(process.env.SLACK_SIGNING_SECRET, {
-  waitForResponse: true
-});
+// An access token (from your Slack app or custom integration - xoxp, xoxb)
+const token = process.env.SLACK_TOKEN;
 
-// https://api.slack.com/events/message.im
-slackEvents.on('message', (event, respond) => {
-  console.log(`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
-  respond(':jorn:');
-});
+const web = new WebClient(token);
 
-// Handle errors (see `errorCodes` export)
-slackEvents.on('error', console.error);
+// This argument can be a channel ID, a DM ID, a MPDM ID, or a group ID
+const conversationId = '#jorn';
 
-// Start a basic HTTP server
-slackEvents.start(port).then(() => {
-  console.log(`server listening on port ${port}`);
-});
+(async () => {
+  // See: https://api.slack.com/methods/chat.postMessage
+  const res = await web.chat.postMessage({ channel: conversationId, text: ':jorn:' });
+
+  // `res` contains information about the posted message
+  console.log('Message sent: ', res.ts);
+})();
