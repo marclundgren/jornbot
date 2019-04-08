d958932cc99eeb3f06008bd615d00c83827a246f
Marc Lundgren <marclundgren2.0@gmail.com>
2019-04-08 10:51:56 -0700

    add stuff

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
-const SLACK_TOKEN = 'xoxp-3508645082-84627622245-507552104784-ce4cfb2d46acbf08dcf0bcf5f25951ad'
-// const SLACK_TOKEN = 'xoxb-3508645082-594903406134-koiyK7cfOSpiB59yCrHRUKuF' // JORN
-const { WebClient } = require('@slack/web-api')
-const web = new WebClient(SLACK_TOKEN)
-// The current date
-const currentTime = new Date().toTimeString()
+const { createEventAdapter } = require('@slack/events-api');
+const port = process.env.PORT || 3000;
 
-const main = function() {
+const slackEvents = createEventAdapter(process.env.SLACK_SIGNING_SECRET, {
+  waitForResponse: true
+});
 
-  // Use the `auth.test` method to find information about the installing user
-  return web.auth.test().then(({ user_id }) => {
-    // Use the `chat.postMessage` method to send a message from this app
-    return web.chat.postMessage({
-      channel: user_id,
-      text: `The current time is ${currentTime}`,
-    }).then(() => {
-      console.log('Message posted!')
-    })
-  })
-}
+// https://api.slack.com/events/message.im
+slackEvents.on('message', (event, respond) => {
+  console.log(`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
+  respond(':jorn:');
+});
 
-main()
+// Handle errors (see `errorCodes` export)
+slackEvents.on('error', console.error);
+
+// Start a basic HTTP server
+slackEvents.start(port).then(() => {
+  console.log(`server listening on port ${port}`);
+});
